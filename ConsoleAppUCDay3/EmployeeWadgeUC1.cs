4be4fa8c83aba76dd81e7e3b30de36ceffe1ce32
Divya <beingdivyadixit@gmail.com>
2022-12-15 23:25:56 +0530

    Employee Attendance(UC1)

---
+++ ConsoleAppUCDay3/EmployeeWadgeUC1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppUCDay3
+{
+    internal class EmployeeWadgeUC1
+    {
+        public static void EmployeeUC1(int empCheck1)
+        {
+            int IS_FULL_TIME = 1;
+            if (empCheck1 == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is not Present");
+            }
+        }
+    }
+}
--- ConsoleAppUCDay3/Program.cs
+++ ConsoleAppUCDay3/Program.cs
@@ -6,7 +6,25 @@ namespace ConsoleAppUCDay3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+            Console.WriteLine(" Enter 0 for welcome program \n Enter 1 for Employee Attendance");
+            int option = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            switch (option)
+            {
+
+                case 0:
+                    Console.WriteLine("Welcome to Employee Wage Computation Program");
+                    break;
+                case 1:
+                    int empCheck1 = random.Next(0, 2);
+                    EmployeeWadgeUC1.EmployeeUC1(empCheck1);
+                    break;
+                default:
+                    Console.WriteLine("Please Input Correct option");
+                    break;
+
+            }
         }
     }
 }

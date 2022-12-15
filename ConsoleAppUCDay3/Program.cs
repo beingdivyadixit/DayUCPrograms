243e821ef6d7906eacee29f84d709fc93110ac7a
Divya <beingdivyadixit@gmail.com>
2022-12-15 23:34:16 +0530

    for Calculating Employee Wage (UC2)

--- ConsoleAppUCDay3/Program.cs
+++ ConsoleAppUCDay3/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleAppUCDay3
         static void Main(string[] args)
         {
 
-            Console.WriteLine(" Enter 0 for welcome program \n Enter 1 for Employee Attendance");
+            Console.WriteLine(" Enter 0 for welcome program \n Enter 1 for Employee Attendance UC1 \n Enter 2 for Calculating Employee Wage UC2");
             int option = Convert.ToInt32(Console.ReadLine());
             Random random = new Random();
             switch (option)
@@ -19,6 +19,10 @@ namespace ConsoleAppUCDay3
                 case 1:
                     int empCheck1 = random.Next(0, 2);
                     EmployeeWadgeUC1.EmployeeUC1(empCheck1);
+                    break;
+                case 2:
+                    int empCheck2 = random.Next(0, 2);
+                    EmployeeWadgeUC2.EmployeeUC2(empCheck2);
                     break;
                 default:
                     Console.WriteLine("Please Input Correct option");

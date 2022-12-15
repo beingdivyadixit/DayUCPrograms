70427b8a2c8886b0e1d269bab7f680a1372ed765
Divya <beingdivyadixit@gmail.com>
2022-12-15 23:19:45 +0530

    Welcome to Employee Wage Computation Program

---
+++ ConsoleAppUCDay3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppUCDay3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}

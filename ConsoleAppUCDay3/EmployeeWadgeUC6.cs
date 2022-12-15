6f57d98dda544d9b2dd7710e2d5baaa353117fed
Divya <beingdivyadixit@gmail.com>
2022-12-15 23:57:59 +0530

    for Calculate Wages till a condition of total working hours of 100 or max days of 20 is reached for a month (UC6)

---
+++ ConsoleAppUCDay3/EmployeeWadgeUC6.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppUCDay3
+{
+    internal class EmployeeWadgeUC6
+    {
+        public static void EmployeeUC6()
+        {
+            int EMP_RATE_PER_HOUR = 20, NUM_OF_WORKING_DAYS = 20, MAX_HRS_IN_MONTH = 100;
+            int totalEmpHr = 0, empHr = 0, totalEmpWadge = 0, totalWorkingDays = 0;
+            while (totalEmpHr <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                Random random = new Random();
+                int empCheck5 = random.Next(0, 3);
+                switch (empCheck5)
+                {
+                    case 0:
+                        empHr = 8;
+                        break;
+
+                    case 1:
+                        empHr = 4;
+                        break;
+                    default:
+                        empHr = 0;
+                        break;
+
+                }
+                totalEmpHr = totalEmpHr + empHr;
+                totalWorkingDays++;
+            }
+            totalEmpWadge = totalEmpHr * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Employee Wadge For A Month:" + totalEmpWadge);
+        }
+    }
+}
--- ConsoleAppUCDay3/Program.cs
+++ ConsoleAppUCDay3/Program.cs
@@ -6,8 +6,7 @@ namespace ConsoleAppUCDay3
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine(" Enter 0 for welcome program \n Enter 1 for Employee Attendance UC1 \n Enter 2 for Calculating Employee Wage UC2 \n Enter 3 for Calculating Employee Wage By Adding Part-time UC3 \n Enter 4 for Calculating Employee Wage By Adding Part-time Using Case UC4 \n Enter 5 Calculating Wages for a Month UC5");
+            Console.WriteLine(" Enter 0 for welcome program \n Enter 1 for Employee Attendance UC1 \n Enter 2 for Calculating Employee Wage UC2 \n Enter 3 for Calculating Employee Wage By Adding Part-time UC3 \n Enter 4 for Calculating Employee Wage By Adding Part-time Using Case UC4 \n Enter 5 Calculating Wages for a Month UC5 \n Enter 6 for Calculate Wages till a condition of total working hours of 100 or max days of 20 is reached for a month UC6");
             int option = Convert.ToInt32(Console.ReadLine());
             Random random = new Random();
             switch (option)
@@ -34,6 +33,9 @@ namespace ConsoleAppUCDay3
                     break;
                 case 5:
                     EmployeeWadgeUC5.EmployeeUC5();
+                    break;
+                case 6:
+                    EmployeeWadgeUC6.EmployeeUC6();
                     break;
                 default:
                     Console.WriteLine("Please Input Correct option");

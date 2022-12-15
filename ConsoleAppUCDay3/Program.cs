afde278c8a72e821d06e8bdb48f26c4f978f0565
Divya <beingdivyadixit@gmail.com>
2022-12-15 23:51:14 +0530

    Calculating Wages for a Month (UC5)

--- ConsoleAppUCDay3/Program.cs
+++ ConsoleAppUCDay3/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleAppUCDay3
         static void Main(string[] args)
         {
 
-            Console.WriteLine(" Enter 0 for welcome program \n Enter 1 for Employee Attendance UC1 \n Enter 2 for Calculating Employee Wage UC2 \n Enter 3 for Calculating Employee Wage By Adding Part-time UC3 \n Enter 4 for Calculating Employee Wage By Adding Part-time Using Case UC4");
+            Console.WriteLine(" Enter 0 for welcome program \n Enter 1 for Employee Attendance UC1 \n Enter 2 for Calculating Employee Wage UC2 \n Enter 3 for Calculating Employee Wage By Adding Part-time UC3 \n Enter 4 for Calculating Employee Wage By Adding Part-time Using Case UC4 \n Enter 5 Calculating Wages for a Month UC5");
             int option = Convert.ToInt32(Console.ReadLine());
             Random random = new Random();
             switch (option)
@@ -31,6 +31,9 @@ namespace ConsoleAppUCDay3
                 case 4:
                     int empCheck4 = random.Next(0, 3);
                     EmployeeWadgeUC4.EmployeeUC4(empCheck4);
+                    break;
+                case 5:
+                    EmployeeWadgeUC5.EmployeeUC5();
                     break;
                 default:
                     Console.WriteLine("Please Input Correct option");

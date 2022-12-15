b74ad05d7ce070e8c6003b3568ba9f8c2f8a2ef5
Divya <beingdivyadixit@gmail.com>
2022-12-15 23:46:09 +0530

    for Calculating Employee Wage By Adding Part-time Using Case (UC4)

---
+++ ConsoleAppUCDay3/EmployeeWadgeUC4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppUCDay3
+{
+    internal class EmployeeWadgeUC4
+    {
+        public static void EmployeeUC4(int empCheck4)
+        {
+            int EMP_RATE_PER_HOUR = 20;
+            int empWadge = 0, empHr = 0;
+
+            switch (empCheck4)
+            {
+                case 0:
+                    empHr = 8;
+                    break;
+
+                case 1:
+                    empHr = 4;
+                    break;
+                default:
+                    empHr = 0;
+                    break;
+            }
+            empWadge = empHr * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wadge:" + empWadge);
+        }
+    }
+}
--- ConsoleAppUCDay3/Program.cs
+++ ConsoleAppUCDay3/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleAppUCDay3
         static void Main(string[] args)
         {
 
-            Console.WriteLine(" Enter 0 for welcome program \n Enter 1 for Employee Attendance UC1 \n Enter 2 for Calculating Employee Wage UC2 \n Enter 3 for Calculating Employee Wage By Adding Part-time UC3");
+            Console.WriteLine(" Enter 0 for welcome program \n Enter 1 for Employee Attendance UC1 \n Enter 2 for Calculating Employee Wage UC2 \n Enter 3 for Calculating Employee Wage By Adding Part-time UC3 \n Enter 4 for Calculating Employee Wage By Adding Part-time Using Case UC4");
             int option = Convert.ToInt32(Console.ReadLine());
             Random random = new Random();
             switch (option)
@@ -27,6 +27,10 @@ namespace ConsoleAppUCDay3
                 case 3:
                     int empCheck3 = random.Next(0, 3);
                     EmployeeWadgeUC3.EmployeeUC3(empCheck3);
+                    break;
+                case 4:
+                    int empCheck4 = random.Next(0, 3);
+                    EmployeeWadgeUC4.EmployeeUC4(empCheck4);
                     break;
                 default:
                     Console.WriteLine("Please Input Correct option");
